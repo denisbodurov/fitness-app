af813c866b142029db98dd9290903a799d7fca20
Denis Bodurov <denisbodurov05@gmail.com>
2024-05-12 02:49:43 +0300

    Changed icon, splash and firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,20 @@
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { getFirestore, initializeFirestore, persistentLocalCache } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
 
 // Configuration
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC3FbeVRVAYmEDoqfjNi9B2KXML1vfyUKM",
-  authDomain: "fitness-app-19411.firebaseapp.com",
-  projectId: "fitness-app-19411",
-  storageBucket: "fitness-app-19411.appspot.com",
-  messagingSenderId: "378514120955",
-  appId: "1:378514120955:web:6e3ec29c92798bcdbf905e",
-  measurementId: "G-2R8710DYN4",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+
 
 export const FIREBASE_APP = initializeApp(FIREBASE_CONFIG);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
